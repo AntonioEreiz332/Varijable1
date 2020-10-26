e3db97818f370ab951343f6e1baa8489d745c6d0
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2020-10-26 09:17:16 +0100

    Add files via upload

---
+++ Varijable1/Varijable1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena; double brojocjena = 0; double zbrojocjena = 0;
+            double prosjek=0;
+            string uputa = "Upiši ocjenu: ";
+            string ispis = "Prosjek je: ";
+            do
+            {
+                Console.Write(uputa);
+                ocjena = Convert.ToInt32(Console.ReadLine());
+                if(ocjena>=1 && ocjena <= 5)
+                {
+                    brojocjena++;
+                    zbrojocjena += ocjena;
+                }else if (ocjena == 0)
+                {
+                    prosjek = zbrojocjena / brojocjena;
+                    Console.Write(ispis);
+                    Console.WriteLine(prosjek);
+                }
+                else
+                {
+                    Console.WriteLine("Molimo unesite uspravnu ocjenu(1-5)!");
+                }
+               
+               
+                
+            } while (ocjena != 0);
+            
+            Console.ReadKey();
+        }
+    }
+}
